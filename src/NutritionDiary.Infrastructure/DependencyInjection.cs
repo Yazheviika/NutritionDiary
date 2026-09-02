f1fc3e93d191ffab7508ebba196956f8065a7649
yazhevika <yazhevikachka@gmail.com>
2026-09-02 14:17:25 +0300

    Feat: wire up DbContext registration via AddInfrastructure

---
+++ src/NutritionDiary.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NutritionDiary.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<NutritionDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
